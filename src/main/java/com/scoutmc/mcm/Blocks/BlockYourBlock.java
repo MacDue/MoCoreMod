9dcace3851785c0c2c19c85e0b5929b11667da03
MacDue <benmaxw@blowitup.com>
2015-04-09 23:53:54 +0100

    Create BlockYourBlock.java

---
+++ src/main/java/com/scoutmc/mcm/Blocks/BlockYourBlock.java
@@ -0,0 +1,25 @@
+
+package com.scoutmc.mcm.Blocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class BlockEnd extends Block
+{
+	private final String name = "YourBlock";
+	public BlockEnd()
+	{
+	super(Material.rock);
+	GameRegistry.registerBlock(this, name);
+	setUnlocalizedName(name);
+//	setCreativeTab();
+the tab you want in there 
+	}
+	
+	public String getName()
+	{
+	return name;
+	}
+    
+}
