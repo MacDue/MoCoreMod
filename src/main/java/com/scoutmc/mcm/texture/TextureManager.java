29a18a1e49c117fa4ebce4a8ee10e205711ffc0f
MacDue <benmaxw@blowitup.com>
2015-04-10 12:17:29 +0100

    Update TextureManager.java

--- src/main/java/com/scoutmc/mcm/texture/TextureManager.java
+++ src/main/java/com/scoutmc/mcm/texture/TextureManager.java
@@ -3,17 +3,19 @@ package com.scoutmc.mcm.texture;
 public class TextureManager {
 	
 	/** Registering Item Textures */
-	
+	//CALL THIS IN YOUR CLIENT PROXY IF YOU DON'T WANT THE SERVER TO CRASH
 	public static void registerItemTextures() {
-		
-		
+		//You need to import some things
+		renderItem.getItemModelMesher().register(ItemManager.YourItem , 0, new ModelResourceLocation("mcm" + ":YourItem", "inventory"));
+
 		
 	}
 	
 	/** Registering Block Textures */
 	
 	public static void registerBlockTextures() {
-		
+		renderItem.getItemModelMesher().register(Item.getItemFromBlock(BlockManage.YourBlock), 0, new ModelResourceLocation("mcm" + ":YourBlock", "inventory"));
+
 		
 		
 	}
